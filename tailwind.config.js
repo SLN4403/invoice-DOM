6b6f742c1f79aed99c7149d02be81b1e5d2c6ffc
Su Lei <suleinandar3044@gmail.com>
2023-10-12 01:04:19 +0630

    invoice app

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      fontFamily: {
+        heading: ['Montserrat', 'sans-serif'],
+        body: ['Rubik', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
